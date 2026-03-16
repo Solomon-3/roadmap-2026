28c3c407ac5880cda48550f6a67a006e69b74d76
Spencer <solomonjass4@gmail.com>
2026-03-16 16:01:32 +0300

    day 3, added request ie POST, and also GET:id eg users/2 and JSON body parsing

---
+++ phase2-backend/week6-express/day2/routes/products.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/",(req, res)=>{
+      res.json([
+          { id: 1, name: "phone"},
+          { id: 2, name: "laptop"}
+      ]);
+});
+
+module.exports = router;
--- phase2-backend/week6-express/day2/routes/users.js
+++ phase2-backend/week6-express/day2/routes/users.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const router = express.Router();
 
@@ -10,5 +11,50 @@ router.get("/", authMiddleware, (req, res) => {
      ]);
 });
 
+
+module.exports = router;
+*/
+
+
+
+
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware = require("../middleware/auth");
+
+let users =[
+         { id: 1, name: "solo"},
+         { id: 2, name: "spencer"}
+];
+
+router.get("/", authMiddleware, (req, res)=>{
+       res.json(users);
+});
+
+router.get("/:id", (req, res)=> {
+       const userId = parseInt(req.params.id);
+       const user =  users.find(u => u.id === userId);
+
+       if (!user){
+             return res.status(404).send("User not found");
+       }
+
+       res.json(user);
+});
+
+router.post("/", (req, res)=>{
+      const newUser= {
+           id: users.length +1,
+           name: req.body.name
+      };
+
+      users.push(newUser);
+
+      res.json({
+          message: "User added",
+          user: newUser
+      });
+});
 
 module.exports = router;
