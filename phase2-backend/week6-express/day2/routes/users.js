7a872e33d6859d4bcf6b4a09ecc346644fdb72d5
Spencer <solomonjass4@gmail.com>
2026-03-17 09:46:36 +0300

    added day 4, added put , delete to the users.js to make it a complete CRUP API

--- phase2-backend/week6-express/day2/routes/users.js
+++ phase2-backend/week6-express/day2/routes/users.js
@@ -17,7 +17,7 @@ module.exports = router;
 
 
 
-
+/*
 const express = require("express");
 const router = express.Router();
 
@@ -27,6 +27,8 @@ let users =[
          { id: 1, name: "solo"},
          { id: 2, name: "spencer"}
 ];
+
+let nextId = 4;
 
 router.get("/", authMiddleware, (req, res)=>{
        res.json(users);
@@ -45,8 +47,9 @@ router.get("/:id", (req, res)=> {
 
 router.post("/", (req, res)=>{
       const newUser= {
-           id: users.length +1,
-           name: req.body.name
+//           id: users.length +1,
+             id: nextId++,
+             name: req.body.name
       };
 
       users.push(newUser);
@@ -56,5 +59,105 @@ router.post("/", (req, res)=>{
           user: newUser
       });
 });
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//DAY 4 FULL CRUD API
+
+
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware = require("../middleware/auth");
+
+let users =[
+         { id: 1, name: "solo"},
+         { id: 2, name: "spencer"}
+];
+
+let nextId = 4;
+
+router.get("/", authMiddleware, (req, res)=>{
+       res.json(users);
+});
+
+router.get("/:id", (req, res)=> {
+       const userId = parseInt(req.params.id);
+       const user =  users.find(u => u.id === userId);
+
+       if (!user){
+             return res.status(404).send("User not found");
+       }
+
+       res.json(user);
+});
+
+router.post("/", (req, res)=>{
+      const newUser= {
+//           id: users.length +1,
+             id: nextId++,
+             name: req.body.name
+      };
+
+      users.push(newUser);
+
+      res.json({
+          message: "User added",
+          user: newUser
+      });
+});
+
+
+router.put("/:id",(req, res)=>{
+      const userId = parseInt(req.params.id);
+      const user = users.find(u=>u.id===userId);
+
+      if (!user){
+          return res.status(404).send("User not found");
+      }
+
+      user.name = req.body.name;
+
+      res.json({
+          message: "User updated",
+          user: user
+      });
+});
+
+
+router.delete("/:id", (req, res)=>{
+       const userId = parseInt(req.params.id);
+       const index = users.findIndex(u=>u.id===userId);
+
+       if (index === -1){
+             return res.status(404).send("User not found");
+       }
+
+       users.splice(index,1);
+
+       res.json({
+            message: "User deleted"
+       });
+
+});
+
 
 module.exports = router;
