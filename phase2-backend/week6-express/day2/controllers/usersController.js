166d73bc8b01e542a853136947f50f7c377c4c5d
Spencer <solomonjass4@gmail.com>
2026-03-30 12:42:34 +0300

    day 7 , added #success#, this ensures consistency eg json, predictability and debugging,

--- phase2-backend/week6-express/day2/controllers/usersController.js
+++ phase2-backend/week6-express/day2/controllers/usersController.js
@@ -1,3 +1,4 @@
+/*
 const fs = require("fs");
 
 let users = JSON.parse(fs.readFileSync("users.json"));
@@ -79,6 +80,132 @@ function deleteUser(req, res) {
     fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
 
     res.json({ message: "User deleted" });
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+};
+
+*/
+
+
+
+
+
+
+// DAY 7
+const fs = require("fs");
+
+let users = JSON.parse(fs.readFileSync("users.json"));
+
+let nextId = users.length + 1;
+
+// GET ALL
+function getUsers(req, res) {
+    res.json({
+       success: true,
+       data: users
+    });
+}
+
+// GET ONE
+function getUserById(req, res) {
+    const userId = parseInt(req.params.id);
+
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({
+               success: false,
+               message: "User not found"
+        });
+    }
+
+    res.json(user);
+}
+
+// CREATE
+function createUser(req, res) {
+    const name = req.body.name;
+
+    if (!name || name.trim() === "") {
+        return res.status(400).json({
+               success: false,
+               message: "Name is required"
+        });
+    }
+
+    const newUser = {
+        id: nextId++,
+        name: name
+    };
+
+    users.push(newUser);
+
+    fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
+
+    res.json({
+         success: true,
+        data: newUser
+     });
+}
+
+// UPDATE
+function updateUser(req, res) {
+    const userId = parseInt(req.params.id);
+    const name = req.body.name;
+
+    if (!name || name.trim() === "") {
+        return res.status(400).json({
+            success: false,
+            message: "Name is required"
+        });
+    }
+
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found"
+        });
+    }
+
+    user.name = name;
+
+    fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
+
+    res.json({
+         success: true,
+         data: user
+    });
+}
+
+// DELETE
+function deleteUser(req, res) {
+    const userId = parseInt(req.params.id);
+
+    const index = users.findIndex(u => u.id === userId);
+
+    if (index === -1) {
+        return res.status(404).json({
+             success: false,
+             message: "User not found"
+        });
+    }
+
+    users.splice(index, 1);
+
+    fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
+
+    res.json({
+         success: true,
+         message: "User deleted"
+    });
 }
 
 module.exports = {
