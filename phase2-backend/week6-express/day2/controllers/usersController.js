8defc0017fc76563718d081996821f8b1c63f72b
Spencer <solomonjass4@gmail.com>
2026-03-31 13:26:30 +0300

    week7, day2, updated our userController.js to actually implement the CRUD with our postgresql db #const pool = require(../db)

--- phase2-backend/week6-express/day2/controllers/usersController.js
+++ phase2-backend/week6-express/day2/controllers/usersController.js
@@ -96,7 +96,7 @@ module.exports = {
 
 
 
-
+/*
 // DAY 7
 const fs = require("fs");
 
@@ -206,6 +206,171 @@ function deleteUser(req, res) {
          success: true,
          message: "User deleted"
     });
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+};
+
+*/
+
+
+
+
+
+// DAY 1 WAS BASICALLY TRYING TO INSTALL POSTGRES AND CREATE USER TABLE AND FINALLY JOIN NODE.JS TO THE DATABASE  REFER TO THE week 7, day1.txt
+//WEEEE7    DAY 2
+
+const pool = require("../db");
+
+// GET ALL
+async function getUsers(req, res) {
+    try{
+        const result = await pool.query("SELECT * FROM users ORDER BY id ASC");
+              res.json({
+                   success: true,
+                   data: result.rows
+               });
+    }
+   catch(err){
+         res.status(500).json({
+             success: false,
+             message: err.message
+         });
+   }
+}
+
+// GET ONE
+async function getUserById(req, res) {
+    const userId = parseInt(req.params.id);
+
+    try {
+        const result = await pool.query(
+              "SELECT * FROM users WHERE id = $1",
+              [userId] 
+        )
+
+        if (result.rows.length === 0 ) {
+              return res.status(404).json({
+                    success: false,
+                    message: "User not found"
+              });
+         }
+
+         res.json({
+             success: true,
+             data: result.rows[0]
+         });
+     }
+     catch (err){
+          res.status(500).json({
+               success: false,
+               message: err.message
+          });
+     }
+}
+
+// CREATE
+async function createUser(req, res) {
+    const name = req.body.name;
+
+    if (!name || name.trim() === "") {
+        return res.status(400).json({
+               success: false,
+               message: "Name is required"
+        });
+    }
+
+    try {
+        const result = await pool.query(
+            "INSERT INTO users (name) VALUES ($1) RETURNING *",
+            [name]
+        );
+
+        res.json({
+            success: true,
+            data: result.rows[0]
+        });
+
+    }
+    catch(err){
+         res.status(500).json({
+              success: false,
+              message: err.message
+         });
+    }
+}
+
+// UPDATE
+async function updateUser(req, res) {
+    const userId = parseInt(req.params.id);
+    const name = req.body.name;
+
+    if (!name || name.trim() === "") {
+        return res.status(400).json({
+            success: false,
+            message: "Name is required"
+        });
+    }
+
+    try{
+         const result = await pool.query(
+              "UPDATE users SET name = $1 WHERE id = $2 RETURNING *",
+              [name, userId]
+         );
+
+         if (result.rows.length === 0) {
+               return res.status(404).json({
+                    success: false,
+                    message: "User not found"
+               });
+         }
+
+         res.json({
+              success: true,
+              data: result.rows[0]
+         });
+    }
+    catch (err){
+         res.status(500).json({
+             success: false,
+             message: err.message
+         });
+    }
+}
+
+// DELETE
+async function deleteUser(req, res) {
+    const userId = parseInt(req.params.id);
+
+    try{
+        const result = await pool.query(
+             "DELETE FROM users WHERE  id =$1 RETURNING *",
+             [userId]
+        );
+
+        if (result.rows.length === 0) {
+             return res.status(404).json({
+                 success: false,
+                 message: "User not found"
+             });
+        }
+
+       res.json({
+            success: true,
+            message: "User deleted"
+       });
+   }
+   catch(err){
+       res.status(500).json({
+           success: false,
+           message: err.message
+       });
+   }
 }
 
 module.exports = {
