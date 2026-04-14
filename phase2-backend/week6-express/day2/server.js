0f61481eef69cb7ebf3045cfee254494574040c7
Spencer <solomonjass4@gmail.com>
2026-04-14 10:02:55 +0300

    week7 day4 , added .env, .gitignore, also middleware ie errorhandler.js, modified the server.js, also modified the db.js to use our environmental variables

--- phase2-backend/week6-express/day2/server.js
+++ phase2-backend/week6-express/day2/server.js
@@ -16,7 +16,7 @@ app.listen(3000, ()=> {
 });
 */
 
-
+/*
 const express = require("express");
 const userRoutes = require("./routes/users");
 const productRoutes = require("./routes/products");
@@ -34,3 +34,44 @@ app.get("/", (req, res) => {
 app.listen(3000, ()=> {
     console.log("Server is running on port 3000");
 });
+*/
+
+
+// wee7 7 day 4
+require("dotenv").config();
+
+const express = require("express");
+const userRoutes = require("./routes/users");
+const productRoutes = require("./routes/products");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+
+app.get("/", (req, res) => {
+    res.json{(
+        success: true,
+        message: "Home Page"
+    )};
+});
+
+//404 handler (very important)
+app.use((req, res)=>{
+    res.status(404).json({
+        success: false,
+        message: "Route not found"
+    });
+})
+
+// global eror handler must be last
+app.use(errorHandler);
+
+
+app.listen(3000, ()=> {
+    console.log("Server is running on port 3000");
+});
+
